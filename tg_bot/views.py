7a644bc985176787d0622700717f38385c1cbdab
Dmitry Puzyrev <puzyrev.dmitry@gmail.com>
2020-04-15 17:53:34 +0300

    new

--- tg_bot/views.py
+++ tg_bot/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import View, FormView
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+import requests
 
 from .models import *
 from .forms import *
