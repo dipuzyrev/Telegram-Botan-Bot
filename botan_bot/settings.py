ab24b8a4a8f73594b184fd382da1404bfeed7f85
Dmitry Puzyrev <puzyrev.dmitry@gmail.com>
2020-04-16 06:36:55 +0300

    disable debug

--- botan_bot/settings.py
+++ botan_bot/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_!#(lk#ldw3dcm@8g*eu=l#rv2!1ikgpz-#psneraty=)8sn#^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['botan-bot.herokuapp.com', '127.0.0.1', 'yourbotan.ru']
 
